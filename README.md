c5de6b405f63f1f43e1296881e5a2197fc419c30
kevin <kevin.sakhuja@gmail.com>
2012-05-01 11:42:44 -0400

    deploy script..

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 S3-Deploy
 =========
 
-Simple script to deploy static assets to S3
+Simple script to deploy static assets to S3 for [wereplaceyou.com](http://wereplaceyou.com)
+### just run the damn deploy script: 
---
+++ deploy.sh
@@ -0,0 +1,8 @@
+echo "Lets do this (::)"
+
+# Deploying Static Files to S3
+php s3up -u AKIAIQK5HEJJVK6HGTAA -p XzEjLGtlMbgFybkEPcVH8rOOrqtNJxqj7lFQFPNm -xzow hackd/wereplaceyou/css/ /Users/kevbook/Sites/wereplaceyou.com/public/css/
+
+php s3up -u AKIAIQK5HEJJVK6HGTAA -p XzEjLGtlMbgFybkEPcVH8rOOrqtNJxqj7lFQFPNm -xzow hackd/wereplaceyou/js/ /Users/kevbook/Sites/wereplaceyou.com/public/css/
+
+php s3up -u AKIAIQK5HEJJVK6HGTAA -p XzEjLGtlMbgFybkEPcVH8rOOrqtNJxqj7lFQFPNm -xzow hackd/wereplaceyou/img/ /Users/kevbook/Sites/wereplaceyou.com/public/css/
